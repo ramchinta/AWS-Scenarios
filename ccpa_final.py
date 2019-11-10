2bde8ea8ae4b94dee4f21a18ea217de66dafb304
Lakshman Chinta <mithrachinta@gmail.com>
2019-11-10 03:42:04 -0500

    Updated ccpa_final.py
    Writes logs into s3

--- ccpa_final.py
+++ ccpa_final.py
@@ -1,29 +1,61 @@
 import pymysql
+import boto3
+import json
 from datetime import datetime
 
 def ccpa():
-    MYSQL_HOST = "database-1.cszampg2p55a.us-east-1.rds.amazonaws.com"
-    MYSQL_USERNAME = "admin"
-    MYSQL_PASSWORD = "Lakshman4"
-    MYSQL_DATABASE = "ccpa"
+    mysql_host = "database-1.cszampg2p55a.us-east-1.rds.amazonaws.com"
+    mysql_username = "admin"
+    mysql_password = "Lakshman4"
+    mysql_database = "ccpa"
+    column = []
+    likecolumn =[]
+    alias = []
+
 
     try:
         '''Connect to the mysql rds database'''
-        mysql_conn = pymysql.connect(host=MYSQL_HOST, port=3306, user=MYSQL_USERNAME, password=MYSQL_PASSWORD,database=MYSQL_DATABASE)
+        mysql_conn = pymysql.connect(host=mysql_host, port=3306, user=mysql_username, password=mysql_password,database=mysql_database)
         mysql_cur = mysql_conn.cursor()
-        '''Execute the sql query which joins ccpa_schema_set and ccpa_pi_keyset and inserts into ccpa_pi_catalog'''
-        mysql_cur.execute("insert into ccpa_pi_catalog (select b.ccpa_category,b.ccpa_pi_name,a.ccpa_system ,a.ccpa_schema,a.ccpa_table ,a.ccpa_column,'Y' as ccpa_pi,'N' as ccpa_for_del,b.ccpa_pi_pct,'Backend' as ccpa_user,current_date() as ccpa_date,1 as ccpa_dataset from ccpa_schema_set as a inner join ccpa_pi_keyset as b on upper(a.ccpa_column) = upper('%'+b.ccpa_pi_alias+'%'))")
-        '''Execute the sql statement which inserts non PI values into ccpa_pi_catalog'''
-        mysql_cur.execute("insert into ccpa_pi_catalog(select '','',a.ccpa_system ,a.ccpa_schema,a.ccpa_table ,a.ccpa_column,'N' as ccpa_pi,'N' as ccpa_for_del,0 as ccpa_pi_pct,'Backend' as ccpa_user,current_date() as ccpa_date,1 as ccpa_dataset from ccpa_schema_set as a left join ccpa_pi_catalog as b on b.ccpa_column is null and b.ccap_table)")
-        '''Write the log file : Successful'''
-        f = open("C:\\Users\\Lakshman\\Documents\\ccpa_logs.txt", "a")
-        f.write(str(datetime.now())+',Backend,Successful'+ '\n')
-        f.close()
+        mysql_cur.execute("select ccpa_column from ccpa.ccpa_schema_set")
+        data = mysql_cur.fetchall()
+        for row in data:
+            for col in row:
+                column.append(col)
+
+        mysql_cur.execute("select ccpa_pi_alias from ccpa.ccpa_pi_keyset")
+        data = mysql_cur.fetchall()
+        for row in data:
+            for col in row:
+                alias.append(col)
+        '''100% match columns'''
+        fullmatch = [B for B in column if B.lower() in (x.lower() for x in alias)]
+        '''Insert 100% match columns'''
+        for i in fullmatch:
+            mysql_cur.execute("insert into ccpa_pi_catalog(select b.ccpa_category,b.ccpa_pi_name,a.ccpa_system ,a.ccpa_schema,a.ccpa_table ,a.ccpa_column,'Y' as ccpa_pi,'N' as ccpa_for_del,avg(b.ccpa_pi_pct + 100),'Backend' as ccpa_user,current_date() as ccpa_date,(select max(ccpa_dataset) from ccpa_pi_catalog)+1 as ccpa_dataset from ccpa_schema_set as a inner join ccpa_pi_keyset as b on a.ccpa_column = '"+i+"' and b.ccpa_pi_alias = '"+i+"'))")
+        column = set(column) - set(alias)
+        '''Generate Like columns'''
+        for i in column:
+            for j in alias:
+                if j.lower() in i.lower():
+                    likecolumn.append(i)
+        '''Insert like columns'''
+        for j in likecolumn:
+            mysql_cur.execute("insert into ccpa_pi_catalog(select b.ccpa_category,b.ccpa_pi_name,a.ccpa_system ,a.ccpa_schema,a.ccpa_table ,a.ccpa_column,'Y' as ccpa_pi,'N' as ccpa_for_del,avg(b.ccpa_pi_pct + 75),'Backend' as ccpa_user,current_date() as ccpa_date,(select max(ccpa_dataset) from ccpa_pi_catalog)+1 as ccpa_dataset from ccpa_schema_set as a inner join ccpa_pi_keyset as b on a.ccpa_column = '"+j+"' and b.ccpa_pi_alias = '"+j+"'))")
+        mysql_cur.execute("insert into ccpa_pi_catalog(ccpa_category,ccpa_pi_name,ccpa_system,ccpa_schema,ccap_table,ccpa_column,ccpa_Pi,ccpa_for_del,ccpa_pi_pct,ccpa_user,ccpa_date,ccpa_dataset)(select ''as ccpa_category,'' as ccpa_pi_name ,a.ccpa_system ,a.ccpa_schema,a.ccpa_table ,a.ccpa_column,'N' as ccpa_pi,'N' as ccpa_for_del,0 as ccpa_pi_pct,'Backend' as ccpa_user,current_date() as ccpa_date,1 as ccpa_dataset from ccpa_schema_set as a left join ccpa_pi_catalog as b on b.ccpa_column is null and b.ccap_table is null)")
+        '''Write the log file to s3 : Successful'''
+        s3 = boto3.resource('s3')
+        obj = s3.Object('axelaardev.com', 'logs/ccpa_pi_catalog/'+str(datetime.now()))
+        data= str(datetime.now())+',Backend,Successful'+ '\n'
+        obj.put(Body=json.dumps(data))
+        print("Y")
+
 
     except:
-        '''Write the log file : Failed'''
-        f = open("C:\\Users\\Lakshman\\Documents\\ccpa_logs.txt", "a")
-        f.write(str(datetime.now())+',Backend,Failed'+ '\n')
-        f.close()
+        '''Write the log file to s3: Failed'''
+        s3 = boto3.resource('s3')
+        obj = s3.Object('axelaardev.com', 'logs/ccpa_pi_catalog/'+str(datetime.now()))
+        data= str(datetime.now())+',Backend,Failed'+ '\n'
+        obj.put(Body=json.dumps(data))
 
 ccpa()
--- ccpa_second.py
+++ ccpa_second.py
@@ -4,15 +4,35 @@ def ccpa():
     MYSQL_HOST = "database-1.cszampg2p55a.us-east-1.rds.amazonaws.com"
     MYSQL_USERNAME = "admin"
     MYSQL_PASSWORD = "Lakshman4"
-    MYSQL_DATABASE = "ccpa"
+    MYSQL_DATABASE = "lakshman"
+    column = []
+    column2 = ['FUR-BO-3174', 'FUR-BO-3175', 'FUR-BO-3176', 'FUR-BO-3177', 'Office Supplies','FUR-BO-3409', 'FUR-BO-3615', 'Furniture','FUR-BO-3616', 'FUR-BO-3617', 'FUR-BO-3618']
+    like = []
+
 
     '''Connect to the mysql rds database'''
     mysql_conn = pymysql.connect(host=MYSQL_HOST, port=3306, user=MYSQL_USERNAME, password=MYSQL_PASSWORD,database=MYSQL_DATABASE)
     mysql_cur = mysql_conn.cursor()
     '''Query to get the column data from CCPA_SCHEMA_SET'''
-    mysql_cur.execute("select ccpa_column from ccpa.ccpa_schema_set")
+    mysql_cur.execute("select productID from lakshman.poc1")
     rows = mysql_cur.fetchall()
+    print(rows)
     for i in rows:
         for j in i:
-            print(j)
+            column.append(j)
+    fullmatch = [B for B in column if B.lower() in (x.lower() for x in column2)]
+    print(fullmatch)
+    for i in fullmatch:
+        print(i)
+    column = set(column) - set(column2)
+    print(list(column))
+    for i in column:
+        for j in column2:
+            if j in i:
+                like.append(i)
+
+    print(like)
+
+
+
 ccpa()
